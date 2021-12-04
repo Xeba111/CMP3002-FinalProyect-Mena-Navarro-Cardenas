f447ecf9ff2cd2bce4b9d8004dc3d20f212946ea
Xeba111 <sebasmena72@yahoo.com>
2021-12-03 23:33:09 -0500

    Ejercicio 7

---
+++ proyectofinal-ejercicio2/main.py
@@ -0,0 +1,25 @@
+contador = 0
+
+def array_subsequences(array, max_index, subarray):
+    if max_index is len(array):
+        if len(subarray) > 2:
+            if check_subsequence(subarray):
+                global contador
+                contador += 1
+    else:
+        array_subsequences(array, max_index+1, subarray)
+        #En este caso se aumenta el caracter que está en el índice length
+        array_subsequences(array, max_index+1, subarray+[array[max_index]])
+
+def check_subsequence(array):
+    #La diferencia entre elementos puede ser 0 pero siempre será positiva por eso el [+1] - [+0]
+    difference = array[1] - array[0]
+    for i in range(len(array)-1):
+        if not array[i+1] - array[i] == difference:
+            return False
+    return True
+
+def respuesta_ejercicio2(array):
+    array_subsequences(array, 0, [])
+    return contador
+
--- proyectofinal-ejercicio2/text_ejercicio2.py
+++ proyectofinal-ejercicio2/text_ejercicio2.py
@@ -3,12 +3,20 @@ import main
 
 class TestCalc(unittest.TestCase):
 
-    def test_ejercicio1(self):
-        self.assertEqual(main.respuesta_ejercicio1([-4, -2, 2, 4], 1, 3, 5), [3, 9, 15, 33])
-        self.assertEqual(main.respuesta_ejercicio1([-4, -2, 2, 4], -1, 3, 5), [-23, -5, 1, 7])
+    def test_ejercicio2_1(self):
+        self.assertEqual(main.respuesta_ejercicio2([2,4,6,8,10]), 7)
+        main.contador = 0
         # self.assertEqual(main.respuesta_ejercicio1([-4, -2, 2, 4], 1, 3, 5), [3, 9, 15, 33])
         # self.assertEqual(main.respuesta_ejercicio1([-4, -2, 2, 4], 1, 3, 5), [3, 9, 15, 33])
         # self.assertEqual(main.respuesta_ejercicio1([-4, -2, 2, 4], 1, 3, 5), [3, 9, 15, 33])
+
+    def test_ejercicio2_2(self):
+        self.assertEqual(main.respuesta_ejercicio2([7,7,7,7,7]), 16)
+        main.contador = 0
+
+    def test_ejercicio2_3(self):
+        self.assertEqual(main.respuesta_ejercicio2([7,7,7,7,7]), 16)
+        main.contador = 0
 
 if __name__ == '__main__':
     unittest.main()
