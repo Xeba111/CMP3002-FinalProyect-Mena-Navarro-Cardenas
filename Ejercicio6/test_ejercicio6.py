2910d565fbbd1de84ad1c6d28a03bf6eeba67623
Cesar Cardenas <cesarcardenasflautor@gmail.com>
2021-12-15 19:34:15 -0500

    Ejercicios

--- Ejercicio6/test_ejercicio6.py
+++ Ejercicio6/test_ejercicio6.py
@@ -1,8 +1,25 @@
 import unittest
 
+import main
+
+
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+
+    def test_something_1(self):
+        self.assertEqual(main.subarrays_odd([5, 4, 4, 5, 1]),8)
+
+    def test_something_2(self):
+        self.assertEqual(main.subarrays_odd([5, 3, 1]),4)
+
+    def test_something_3(self):
+        self.assertEqual(main.subarrays_odd([1,2, 3]),4)
+
+    def test_something_4(self):
+        self.assertEqual(main.subarrays_odd([5,5,1,2]),6)
+
+    def test_something_5(self):
+        self.assertEqual(main.subarrays_odd([2,2,1,3,33]),8)
+
 
 if __name__ == '__main__':
     unittest.main()
--- Ejercicio9/test_ejercicio9.py
+++ Ejercicio9/test_ejercicio9.py
@@ -1,8 +1,13 @@
 import unittest
 
+import main
+
+
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_something_1(self):
+        self.assertEqual(main.list1.insertVal(main.m5),{1,2,3,4,5})
+    def test_something_2(self):
+        self.assertEqual(main.list2.insertVal(main.n5),{1,4,5,4,9})
 
 if __name__ == '__main__':
     unittest.main()
